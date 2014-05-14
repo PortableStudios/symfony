2df2f8bb02345fb803f3a127894e6c9effe533cf
Jerram Watters <jerramw@gmail.com>
2014-05-14 11:14:13 +1000

    FosUserBundle
    Add FosUserBundle
    Add separate AcmeUserBundle, to separeate users from business bundles
    Following along with
    https://github.com/FriendsOfSymfony/FOSUserBundle/blob/master/Resources/doc/index.md
    With the excepetion of adding Bundle definitiion file:
    /var/vhosts/symfony/src/Acme/UserBundle/AcmeUserBundle.php

---
+++ src/Acme/UserBundle/AcmeUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+namespace Acme\UserBundle;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
+#use Doctrine\Bundle\MongoDBBundle\DependencyInjection\Compiler\DoctrineMongoDBMappingsPass;
+#use Doctrine\Bundle\CouchDBBundle\DependencyInjection\Compiler\DoctrineCouchDBMappingsPass;
+class AcmeUserBundle extends Bundle {
+}
